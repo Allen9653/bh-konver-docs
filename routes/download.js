33eb280c884a59b454de1e1d11ca4a205f3a71d9
Allen9653 <alenj9653@gmail.com>
2025-10-23 07:28:26 +0200

    Clean up comments in download route
    Removed comments about file existence check and download process.

--- routes/download.js
+++ routes/download.js
@@ -9,17 +9,14 @@ router.get("/:filename", (req, res) => {
   const { filename } = req.params;
   const filePath = path.join(__dirname, "../converted", filename);
 
-  // Provjera da li fajl postoji
   if (!fs.existsSync(filePath)) {
     return res.status(404).json({ error: "Fajl nije pronađen." });
   }
 
-  // Slanje fajla korisniku
   res.download(filePath, filename, (err) => {
     if (err) {
       console.error("Greška pri preuzimanju:", err);
     } else {
-      // Automatsko brisanje nakon preuzimanja
       fs.unlink(filePath, (unlinkErr) => {
         if (unlinkErr) {
           console.error("Greška pri brisanju fajla:", unlinkErr);
@@ -32,3 +29,4 @@ router.get("/:filename", (req, res) => {
 });
 
 module.exports = router;
+
