7d73b9fd29e396a01dbac5daf35f71071b4e21e4
Allen9653 <alenj9653@gmail.com>
2025-10-23 07:33:03 +0200

    Fix order of middleware registration for download route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const downloadRoute = require("./routes/download");
-app.use("/download", downloadRoute);
 const paypalRoute = require("./routes/payment/paypal");
 const paypalWebhook = require("./routes/webhook/paypal-confirmation");
 
+app.use("/download", downloadRoute);
 app.use("/payment", paypalRoute);
 app.use("/webhook", paypalWebhook);
