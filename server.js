259d094c0b60fdf7dc8fe5381cfc0869af758507
Allen9653 <alenj9653@gmail.com>
2025-10-23 07:30:55 +0200

    Add PayPal routes for payment and webhook

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const downloadRoute = require("./routes/download");
 app.use("/download", downloadRoute);
+const paypalRoute = require("./routes/payment/paypal");
+const paypalWebhook = require("./routes/webhook/paypal-confirmation");
+
+app.use("/payment", paypalRoute);
+app.use("/webhook", paypalWebhook);
